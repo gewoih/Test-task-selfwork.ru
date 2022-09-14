73e5cc06b705f9d15b1069b2d17f39fb697b1a9d
Никита Раненко <nranenko@bk.ru>
2022-09-14 23:21:25 +0300

    Добавьте файлы проекта.

---
+++ Controllers/RGDialogsClientsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Test_task_selfwork.ru.Models;
+
+namespace Test_task_selfwork.ru.Controllers
+{
+	[Controller]
+	[Route("dialogs")]
+	public class RGDialogsClientsController : Controller
+	{
+		[HttpGet]
+        [Route("findDialogGuidByClients")]
+		public Guid FindDialogGuidByClients(List<Guid> clientsGuids)
+		{
+			List<RGDialogsClients> dialogs = new RGDialogsClients().Init();
+
+			var dialogGuids = dialogs.Select(d => d.IDRGDialog).Distinct();
+
+			foreach (var dialogGuid in dialogGuids)
+			{
+				IEnumerable<Guid> dialogClientsGuids = dialogs
+														.Where(d => d.IDRGDialog == dialogGuid)
+														.Select(d => d.IDClient);
+
+				int count = clientsGuids.Except(dialogClientsGuids).Count();
+
+				if (count == 0)
+					return dialogGuid;
+			}
+
+			return Guid.Empty;
+		}
+	}
+}
